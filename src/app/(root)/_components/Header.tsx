381f0e6b056a378a61167f036d4c00d24d970452
jaeyup06 <jaeyup06@gmail.com>
2024-09-30 11:10:26 +0900

    Header 완성

--- src/app/(root)/_components/Header.tsx
+++ src/app/(root)/_components/Header.tsx
@@ -1,7 +1,29 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import LogInModal from "./LogInModal";
+
 function Header() {
+  const [isLogIn, setIsLogIn] = useState(false);
+
   return (
-    <header className="text-3xl">여긴 header</header>
-  )
+    <>
+      {isLogIn && <LogInModal />}
+      <header className="h-16 border-b flex justify-between items-center px-7 font-light">
+        <div className="flex items-center gap-20">
+          <Link href={"/"} className="text-2xl font-bold">
+            발랑
+          </Link>
+          <Link href={"/brands"}>BRANDS</Link>
+        </div>
+        <div className="flex gap-5">
+          <Link href={"/sign-up"}>회원가입</Link>
+          <button onClick={() => setIsLogIn(!isLogIn)}>로그인</button>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default Header;
